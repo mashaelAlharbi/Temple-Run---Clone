97f98f2451f2d9d12aebfe2295d0d84f32f5d7bb
mashaelAlharbi <mashael.dhaifallah@gmail.com>
2023-02-02 14:07:02 +0300

    added scripts files

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private bool turnLeft, turnRight;
+    public float speed = 7.0f;
+    private CharacterController myCharacterController;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        myCharacterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        turnLeft = Input.GetKeyDown(KeyCode.A);
+        turnRight = Input.GetKeyDown(KeyCode.D);
+
+        if (turnLeft)
+            transform.Rotate(new Vector3(0f, -90f, 0f));
+        else if (turnRight)
+            transform.Rotate(new Vector3(0f, 90f, 0f));
+
+        myCharacterController.SimpleMove(new Vector3(0f, 0f, 0f));
+        myCharacterController.Move(transform.forward * speed * Time.deltaTime);
     }
 }
